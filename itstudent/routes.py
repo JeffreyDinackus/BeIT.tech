ad5298cc63486f56c351feedc43cf802e1d1c251
BuildTools <unconfigured@null.spigotmc.org>
2023-01-28 13:12:43 -0500

    FIXED HTML and routers

--- itstudent/routes.py
+++ itstudent/routes.py
@@ -7,7 +7,7 @@ from flask import render_template
 @app.route('/homepage')
 @app.route('/home')
 def index():
-    return render_template(index.html)
+    return render_template('index.html')
 
 
 @app.route('/hello')
