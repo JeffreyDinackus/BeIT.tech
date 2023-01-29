f4f12cb355b19d5365af41b47b273b5b0ac2905b
JeffreyDinackus <102326113+JeffreyDinackus@users.noreply.github.com>
2023-01-28 22:47:36 -0500

    Update __init__.py

--- itstudent/__init__.py
+++ itstudent/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+database = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 
 if __name__ == '__main__':
@@ -10,4 +16,6 @@ if __name__ == '__main__':
     #Use the http routes as defined in auth and routes as Blueprints
     app.register_blueprint(routes, url_prefix ='/')
     app.register_blueprint(auth, url_prefix ='/')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{DB_NAME}'
+    database.init_app(app)
     app.run(debug=True)
